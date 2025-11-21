af457591a17c662e91742afb52bddb28397921c8
Juansh96 <juansh96@hotmail.com>
2025-11-21 15:37:03 -0500

    font config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-
-const page = () => {
-  return <h1>Hello World</h1>;
+const Home = () => {
+  return (
+    <>
+      <h1 className="h1-bold">Hello World</h1>
+      <h1 className="h1-bold font-space-grotesk">Hello World</h1>
+    </>
+  );
 };
 
-export default page;
+export default Home;
