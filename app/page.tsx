2ea9c957b319d09db6c666d4975259f23e7d421a
Juansh96 <juansh96@hotmail.com>
2025-11-24 23:37:43 -0500

    theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const Home = () => {
   return (
     <>
       <h1 className="h1-bold">Hello World</h1>
-      <h1 className="h1-bold font-space-grotesk">Hello World</h1>
     </>
   );
 };
